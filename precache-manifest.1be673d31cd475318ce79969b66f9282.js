3ad3d7994a2fd9505f28e35cc75c62913179a917
brianargel <oroboruo566@gmail.com>
2020-06-13 21:27:55 -0500

    Updates

---
+++ precache-manifest.1be673d31cd475318ce79969b66f9282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c948a863df672619dca85d818cb2b4d",
+    "url": "/HackingBrian584/appnoticia/index.html"
+  },
+  {
+    "revision": "7ca16d57a36f3457516d",
+    "url": "/HackingBrian584/appnoticia/static/css/main.7ba43606.chunk.css"
+  },
+  {
+    "revision": "d8020ad95fb76c38b7eb",
+    "url": "/HackingBrian584/appnoticia/static/js/2.1346f748.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HackingBrian584/appnoticia/static/js/2.1346f748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca16d57a36f3457516d",
+    "url": "/HackingBrian584/appnoticia/static/js/main.d2515575.chunk.js"
+  },
+  {
+    "revision": "0a9297428d594dd225a4",
+    "url": "/HackingBrian584/appnoticia/static/js/runtime-main.927bdd9a.js"
+  }
+]);
